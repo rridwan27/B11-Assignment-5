fff3890f6b515627119b3812ffec743a8dc20232
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-03-27 17:04:58 +0600

    devboard completed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/* To main.html */
+
+const secondPage = document.querySelector(".second-page");
+
+secondPage.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "./main.html";
+});
+
+/* Theme Change */
+
 const themeBtn = document.querySelector(".theme-btn");
 const body = document.body;
 
@@ -16,6 +27,8 @@ themeBtn.addEventListener("click", function () {
   currentColorIndex = (currentColorIndex + 1) % colors.length;
   body.style.backgroundColor = colors[currentColorIndex];
 });
+
+/* Current Date and Time */
 
 function updateDateTime() {
   const dateTime = document.querySelector(".date-time");
@@ -40,11 +53,15 @@ function updateDateTime() {
 
 updateDateTime();
 
+/* Task Completion Section */
+
 const completeButtons = document.querySelectorAll(".complete-btn");
 const completedTaskElement = document.querySelector(".completed-task");
 const taskCompletedList = document.querySelector(".task-completed ul");
+const assignedTasks = document.querySelector(".assigned-tasks");
 
 let completedCount = parseInt(completedTaskElement.textContent) || 0;
+let assignedTasksCount = parseInt(assignedTasks.textContent) || 0;
 
 completeButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -57,25 +74,44 @@ completeButtons.forEach((button) => {
       button.classList.remove("bg-[#3752FD]");
       button.classList.add("bg-gray-400");
 
+      alert("Board Updated Successfully");
+
       completedCount += 1;
       completedTaskElement.textContent = completedCount;
 
+      assignedTasksCount -= 1;
+      assignedTasks.textContent = assignedTasksCount;
+
+      const allDisabled = [...completeButtons].every(
+        (button) => button.disabled
+      );
+      if (allDisabled) {
+        alert("Congrats!! All tasks are finished!!!");
+      }
+
       const listItem = document.createElement("li");
-      listItem.textContent = `${taskTitle} - Completed on ${new Date().toLocaleDateString()}`;
-      listItem.classList.add("py-2", "text-md", "text-black", "font-medium");
+      listItem.textContent = `You have completed the task ${taskTitle} - Completed at ${new Date().toLocaleTimeString()}`;
+      listItem.classList.add(
+        "py-2",
+        "px-1",
+        "mt-2",
+        "rounded-md",
+        "text-md",
+        "text-black",
+        "font-medium",
+        "bg-[#F4F7FF]"
+      );
       taskCompletedList.appendChild(listItem);
     }
   });
 });
 
+/* Clear History */
+
 const clearButton = document.querySelector(".clear-btn");
 
 function clearHistory() {
-  while (taskCompletedList.firstChild) {
-    taskCompletedList.removeChild(taskCompletedList.firstChild);
-  }
-  const emptyItem = document.createElement("li");
-  taskCompletedList.appendChild(emptyItem);
+  taskCompletedList.innerHTML = "";
 }
 
 clearButton.addEventListener("click", clearHistory);
